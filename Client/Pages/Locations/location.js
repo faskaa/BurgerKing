2a0227333d31f832aa1f0c253dc0c0301fe0ad3f
faskaa <fazilnb@code.edu.az>
2024-02-07 23:20:05 +0400

    Location page's javascript added

---
+++ Client/Pages/Locations/location.js
@@ -0,0 +1,28 @@
+const locationElements = document.querySelectorAll('.locations .bottom .location');
+
+locationElements.forEach(function(location) {
+    location.addEventListener('click', function() {
+        this.classList.toggle('active');
+
+        locationElements.forEach(function(element) {
+            if (element !== this && element.classList.contains('active')) {
+                element.classList.remove('active');
+            }
+        }, this);
+    });
+});
+
+locationElements.forEach(function(location) {
+    const heartButton = location.querySelector('.location-top .right button');
+
+    heartButton.addEventListener('click', function(e) {
+        e.stopPropagation(); 
+
+        this.classList.toggle('active');
+
+        const heartImgs = this.querySelectorAll('img');
+        heartImgs.forEach(function(img) {
+            img.classList.toggle('active');
+        });
+    });
+});
