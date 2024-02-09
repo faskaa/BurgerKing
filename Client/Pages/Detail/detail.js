1b8e0f32fa215d26dde901ff86bd34995a382abd
faskaa <fazilnb@code.edu.az>
2024-02-10 02:48:20 +0400

    Detail Page completed

---
+++ Client/Pages/Detail/detail.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.querySelectorAll('.size .right button');
+    const orderDiv = document.querySelector('.section-detail-add-order .add-order .order')
+    const addButtons = document.querySelectorAll('.order .order-right button');
+
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            buttons.forEach(function(btn) {
+                btn.classList.remove('clicked');
+            });
+
+            button.classList.toggle('clicked');
+        });
+
+        addButtons.forEach(function(button){
+            button.addEventListener('click', function(){
+                orderDiv.classList.add('active')
+            })
+        })
+
+    });
+    
+});
+
+
+function updateNumber(operation) {
+    var numberElement = document.getElementById('numberDisplay');
+    var decrementBtn = document.getElementById('decrementBtn');
+    var currentNumber = parseInt(numberElement.innerText);
+
+    if (operation === '+') {
+        currentNumber++;
+    } else if (operation === '-') {
+        currentNumber = Math.max(1, currentNumber - 1);
+    }
+
+    numberElement.innerText = currentNumber;
+
+    decrementBtn.disabled = (currentNumber === 1);
+}
+
+document.getElementById('incrementBtn').addEventListener('click', function() {
+    updateNumber('+');
+});
+
+document.getElementById('decrementBtn').addEventListener('click', function() {
+    updateNumber('-');
+});
+
+
+
